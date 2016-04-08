73471ff9f351b874c00ae117d6b09e8dc8931a5e
derrickturk <dwt@terminusdatascience.com>
2016-04-08 16:08:52 -0500

    Correct economic limit time determination for declines with buildup (closes #4).

--- R/eur.R
+++ R/eur.R
@@ -19,9 +19,13 @@
 
 arps.t.el <- function(decl, q.limit)
 {
+    min.time <- if (is(decl, 'buildup'))
+        decl$time.to.peak
+    else
+        0
     nlminb(10,   # initial guess 10 periods
             function (t) ((arps.q(decl, t) - q.limit) ^ 2), # cost function
-            lower=0,   # minimum 0
+            lower=min.time,   # minimum 0 or time-to-peak
             upper=1e6  # 1 million [time units]
     )$par
 }
